077ca6e202302d3acc33fcfdef894d9789c2a740
Janindu Malshan <janindumalshan50@gmail.com>
2025-09-28 17:59:37 +0530

    fixed code

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM debian:latest
-
-RUN apt update && apt upgrade -y
-RUN apt install python3-pip -y
-RUN pip3 install -U pip
-COPY . /app
-WORKDIR /app
-RUN pip3 install -U -r requirements.txt
-CMD python3 main.py
---
+++ app.py
@@ -0,0 +1,59 @@
+import os
+import time
+import schedule
+import requests
+from bs4 import BeautifulSoup
+
+BOT_TOKEN = "" # Add telegram bot token here
+
+def func():
+    try:
+        url = "https://hirunews.lk/"
+
+        # Send request
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        # Extract image URL
+        image = soup.select_one(".image-wrp img")
+        image_url = image["src"] if image else None
+
+        lel = open("./text.txt","r+")
+        check = lel.readline()
+        
+        if check != image_url:
+
+            # Extract title
+            title = soup.select_one(".card-title-v1")
+            title_text = title.get_text(strip=True) if title else None
+
+            # Extract description
+            description = soup.select_one(".description")
+            description_text = description.get_text(strip=True) if description else None
+
+            # Extract time (second span inside .update-wrp-lg)
+            time_span = soup.select_one(".update-wrp-lg span:nth-of-type(2)")
+            time_text = time_span.get_text(strip=True) if time_span else None
+
+            cap1 = f"📮 <b>{title_text}</b>"
+            cap2 = f"✍️ {description_text} \n\n📅 {time_text} \n\n🇱🇰 Powered by hirunews.lk"
+
+
+            tg1 = f"https://api.telegram.org/bot{BOT_TOKEN}/sendPhoto?chat_id=-1001530519480&photo={image_url}&caption={cap1}&parse_mode=html"
+            tg2 = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id=-1001530519480&text={cap2}&parse_mode=html"
+
+            requests.get(tg1)
+            requests.get(tg2)
+
+            with open('./text.txt', 'w') as f:
+                f.write(image_url)
+
+    except Exception as e:
+            print(e)
+            pass
+
+schedule.every(0.1).minutes.do(func)
+  
+while True:
+    schedule.run_pending()
+    time.sleep(1)
